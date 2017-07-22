a747079f1a9c1947a1f009419a160913e66fe80e
kevin <kevin.robbins14@gmail.com>
2017-07-22 13:03:29 -0400

    Initial analysis of block objects

--- BlockStats.py
+++ BlockStats.py
@@ -3,5 +3,10 @@ from blockchain import statistics
 
 block1 = GetBlock()
 stats = statistics.get()
-print(stats.estimated_transaction_volume_usd)
+bitprice = stats.market_price_usd
 print(stats.market_price_usd)
+
+import googlefinance
+import json
+with open('data.json','w') as outfile:
+    json.dump({'BitPrice':bitprice, 'Stock':googlefinance.getQuotes(['.INX','.DJI'])}, outfile, indent=4)
