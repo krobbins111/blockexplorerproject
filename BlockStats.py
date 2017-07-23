f5079c742cdd437e48165e3fd6ce46f155ca9aa6
kevin <kevin.robbins14@gmail.com>
2017-07-22 20:37:46 -0400

    Initial analysis of block objects

--- BlockStats.py
+++ BlockStats.py
@@ -1,7 +1,12 @@
+from MyClient import connect
+
+
 from blockgetter import GetBlock
 from blockchain import statistics
 
-block1 = GetBlock()
+connect('10.0.0.141',3000)
+
+block1 = GetBlock() #For later real-time block info
 stats = statistics.get()
 bitprice = stats.market_price_usd
 print(stats.market_price_usd)
---
+++ MyClient.py
@@ -0,0 +1,30 @@
+import websocket, http.client, sys, asyncore
+
+
+def connect(server, port):
+    print("connecting to: %s:%d" % (server, port))
+
+    conn = http.client.HTTPConnection(server + ":" + str(port))
+    conn.request('POST', '/test', '{"data":1}')
+    resp = conn.getresponse()
+    #hskey = resp.read().split(':')[0]
+
+    ws = websocket.WebSocket(
+        'ws://' + server + ':' + str(port),
+        onopen=_onopen,
+        onmessage=_onmessage,
+        onclose=_onclose)
+
+    return ws
+
+
+def _onopen():
+    print("opened!")
+
+
+def _onmessage(msg):
+    print("msg: " + str(msg))
+
+
+def _onclose():
+    print("closed!")
